63f56be1e7a799f9379830843594394eb483ce61
24LP3420 24LP3420 <24LP3420@G303-CC0138.local>
2025-04-11 09:31:14 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -102,65 +102,70 @@ export default function Home() {
   }, [currentWeather])
 
   return (
-    <div className="flex h-screen">
-      <div className="bg-[#F3F4F6] w-1/2">
-        <div className="mt-10 ml-10">
-          <div className="flex bg-white backdrop-blur-xs rounded-full px-6 py-4 w-1/2 gap-4">
-            <img src="icons/search.svg" />
-            <input placeholder="Search..." onChange={handleSearch} value={value} className="focus:outline-none placeholder: font-bold text-3xl" />
+    <div className="relative flex justify-center items-center">
+      <div className="flex h-screen w-full">
+        <div className="bg-[#F3F4F6] w-1/2">
+          <div className="mt-10 ml-10">
+            <div className="flex bg-white backdrop-blur-xs rounded-full px-6 py-4 w-1/2 gap-4 relative z-5">
+              <img src="icons/search.svg" />
+              <input placeholder="Search..." onChange={handleSearch} value={value} className="focus:outline-none placeholder: font-bold text-3xl" />
+            </div>
+            <div className={`absolute z-10 w-[410px] rounded-3xl object-fit p-6 mt-3 bg-white backdrop-blur-xs ${filteredCities.length === 0 ? "invisible" : "visible"}`}>
+              {filteredCities.map((city, index) => {
+                return <div key={nanoid()} onClick={() => handleSelectedCity(index)} className="text-2xl font-bold flex gap-4 pb-2"><span><img src="/icons/nightpin.svg" /></span>{city}</div>
+              })}
+            </div>
           </div>
-          <div className={`absolute z-10 w-[410px] rounded-3xl object-fit p-6 mt-3 bg-white backdrop-blur-xs ${filteredCities.length === 0 ? "invisible" : "visible"}`}>
-            {filteredCities.map((city, index) => {
-              return <div key={nanoid()} onClick={() => handleSelectedCity(index)} className="text-2xl font-bold flex gap-4 pb-2"><span><img src="/icons/nightpin.svg" /></span>{city}</div>
-            })}
+          <div className="rounded-4xl bg-white/75 backdrop-blur-xs w-1/2 object-fit mx-auto mt-25 px-12 py-16">
+            <div className="flex justify-between">
+              <div>
+                <p className="text-gray-400 text-leg font-medium">{currentWeather?.forecast.forecastday[0].date}</p>
+                <h1 className="text-5xl font-extrabold">{currentWeather?.location.name}</h1>
+              </div>
+              <img src="/icons/daypin.svg" />
+            </div>
+            <div className="w-[277px] h-[277px] mx-auto rounded-full">
+              <img src={`${dayImage}`} className="w-[264px] h-[264px] mt-6" />
+            </div>
+            <div className="mt-3 mb-12">
+              <h1 className="py-3 font-extrabold text-9xl text-transparent bg-clip-text bg-linear-to-bl from-[#111827] to-[#6B7280]">{`${currentWeather?.forecast.forecastday[0].hour[12].temp_c}°`}</h1>
+              <p className="font-extrabold text-2xl text-[#FF8E27]">{currentWeather?.forecast.forecastday[0].hour[12].condition.text}</p>
+            </div>
+            <div className="flex justify-between">
+              <img src="/icons/dayhouse.svg" />
+              <img src="/icons/daybotpin.svg" />
+              <img src="/icons/dayheart.svg" />
+              <img src="/icons/dayuser.svg" />
+            </div>
           </div>
         </div>
-        <div className="rounded-4xl bg-white/75 backdrop-blur-xs w-1/2 object-fit mx-auto mt-25 px-12 py-16">
-          <div className="flex justify-between">
-            <div>
-              <p className="text-gray-400 text-leg font-medium">{currentWeather?.forecast.forecastday[0].date}</p>
-              <h1 className="text-5xl font-extrabold">{currentWeather?.location.name}</h1>
+        <div className="bg-[#0F141E] w-1/2 text-white">
+          <div className="mt-[202px] rounded-4xl bg-[#1F2937]/75 backdrop-blur-xs w-1/2 object-fit mx-auto mt-25 px-12 py-16 relative z-5">
+            <div className="flex justify-between">
+              <div>
+                <p className="text-gray-400 text-leg font-medium">{currentWeather?.forecast.forecastday[0].date}</p>
+                <h1 className="text-5xl font-extrabold">{currentWeather?.location.name}</h1>
+              </div>
+              <img src="/icons/nightpin.svg" />
             </div>
-            <img src="/icons/daypin.svg" />
-          </div>
-          <div className="w-[277px] h-[277px] mx-auto rounded-full">
-            <img src={`${dayImage}`} className="w-[264px] h-[264px] mt-6" />
-          </div>
-          <div className="mt-3 mb-12">
-            <h1 className="py-3 font-extrabold text-9xl text-transparent bg-clip-text bg-linear-to-bl from-[#111827] to-[#6B7280]">{`${currentWeather?.forecast.forecastday[0].hour[12].temp_c}°`}</h1>
-            <p className="font-extrabold text-2xl text-[#FF8E27]">{currentWeather?.forecast.forecastday[0].hour[12].condition.text}</p>
-          </div>
-          <div className="flex gap-16">
-            <img src="/icons/dayhouse.svg" />
-            <img src="/icons/daybotpin.svg" />
-            <img src="/icons/dayheart.svg" />
-            <img src="/icons/dayuser.svg" />
+            <div className="w-[277px] h-[277px] mx-auto">
+              <img src={`${nightImage}`} className="w-[264px] h-[264px] mt-6 rounded-full" />
+            </div>
+            <div className="mt-3 mb-12">
+              <h1 className="py-3 font-extrabold text-9xl text-transparent bg-clip-text bg-linear-to-bl from-gray-100 to-gray-700">{`${currentWeather?.forecast.forecastday[0].hour[21].temp_c}°`}</h1>
+              <p className="font-extrabold text-2xl text-[#777CCE]">{currentWeather?.forecast.forecastday[0].hour[21].condition.text}</p>
+            </div>
+            <div className="flex justify-between">
+              <img src="/icons/nighthouse.svg" />
+              <img src="/icons/botnightpin.svg" />
+              <img src="/icons/nightheart.svg" />
+              <img src="/icons/nightuser.svg" />
+            </div>
           </div>
         </div>
       </div>
-      <div className="bg-[#0F141E] w-1/2 text-white">
-        <div className="mt-[202px] rounded-4xl bg-[#1F2937]/75 backdrop-blur-xs w-1/2 object-fit mx-auto mt-25 px-12 py-16">
-          <div className="flex justify-between">
-            <div>
-              <p className="text-gray-400 text-leg font-medium">{currentWeather?.forecast.forecastday[0].date}</p>
-              <h1 className="text-5xl font-extrabold">{currentWeather?.location.name}</h1>
-            </div>
-            <img src="/icons/nightpin.svg" />
-          </div>
-          <div className="w-[277px] h-[277px] mx-auto">
-            <img src={`${nightImage}`} className="w-[264px] h-[264px] mt-6 rounded-full" />
-          </div>
-          <div className="mt-3 mb-12">
-            <h1 className="py-3 font-extrabold text-9xl text-transparent bg-clip-text bg-linear-to-bl from-gray-100 to-gray-700">{`${currentWeather?.forecast.forecastday[0].hour[21].temp_c}°`}</h1>
-            <p className="font-extrabold text-2xl text-[#777CCE]">{currentWeather?.forecast.forecastday[0].hour[21].condition.text}</p>
-          </div>
-          <div className="flex gap-16">
-            <img src="/icons/nighthouse.svg" />
-            <img src="/icons/botnightpin.svg" />
-            <img src="/icons/nightheart.svg" />
-            <img src="/icons/nightuser.svg" />
-          </div>
-        </div>
+      <div className="absolute flex justify-center items-center">
+        <div className="rounded-full h-200 w-200 border border-white"></div>
       </div>
     </div>
   );
